945b805a27c1ea98d4f482e9ac6107f283074100
Andrey Ivanov <aivanov@vp.ru>
2020-04-15 22:35:19 +0600

    Add delete request

--- routes/todo.js
+++ routes/todo.js
@@ -46,7 +46,16 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
     try {
+        const todos = await Todo.findAll({
+            where: {
+                id: +req.params.id
+            }
+        });
+        console.log({ todos });
+        const todo = todos[0];
+        await todo.destroy();
 
+        res.status(204).json({});
     } catch (e) {
         console.log(e);
         res.status(500).json({
